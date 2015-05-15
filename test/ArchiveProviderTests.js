93b029949f242b6cec3879f40a28f9e8b95acf12
Joe Wilm <joe@gameclosure.com>
2015-05-14 18:29:38 -0700

    Implement ArchiveProvider.getAttributes
    The return types for these methods were originally to just be specified
    by JSDoc comments, but it's pretty handy having real classes.

--- test/ArchiveProviderTests.js
+++ test/ArchiveProviderTests.js
@@ -1,5 +1,10 @@
 var ArchiveProvider = require(__lib + '/providers/ArchiveProvider');
 var path = require('path');
+var Application = require(lib('Application'));
+var ApplicationSummary = require(lib('ApplicationSummary'));
+
+// TODO abstract provider tests from any concrete provider. This is achieveable
+// by specifying some setup/teardown functions for various test types.
 
 describe('ArchiveProvider', function () {
   before(function () {
@@ -9,7 +14,7 @@ describe('ArchiveProvider', function () {
     });
   });
 
-  describe('getApps', function () {
+  describe('#getApps', function () {
     it('returns a promise', function () {
       assert(this.provider.getApps() instanceof Promise);
     });
@@ -20,11 +25,11 @@ describe('ArchiveProvider', function () {
       }).then(done, done);
     });
 
-    it('resolves an array of app metadata objects', function (done) {
+    it('resolves an array of ApplicationSummary', function (done) {
       this.provider.getApps().then(function (apps) {
         assert(Array.isArray(apps));
         apps.forEach(function (app) {
-          assert.equal(app.toString(), '[object Object]');
+          assert(app instanceof ApplicationSummary);
           assert(app.id);
           assert(app.name);
           assert(app.description);
@@ -34,6 +39,32 @@ describe('ArchiveProvider', function () {
       }).then(done, done);
     });
 
+  });
+
+  describe('#getAttributes', function () {
+    var appName = 'shooter';
+    it('returns a promise', function () {
+      assert(this.provider.getAttributes(appName) instanceof Promise);
+    });
+
+    it('resolves to an Application', function () {
+      this.provider.getAttributes(appName).then(function (attrs) {
+        assert(attrs instanceof Application);
+      });
+
+    });
+
+    it('contains the correct data', function () {
+      this.provider.getAttributes(appName).then(function (attrs) {
+        assert(attrs.id);
+        assert(attrs.name);
+        assert(attrs.description);
+        assert(attrs.icon);
+        assert(attrs.splash);
+        assert(attrs.versions);
+        assert(Array.isArray(attrs.versions));
+      });
+    });
   });
 
 });
